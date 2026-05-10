32f4eb17edb96096a70c0a93f5bf351dee451f06
NimeshGhag <ghagnimesh490@gmail.com>
2026-05-10 19:04:49 +0530

    feat(auth): implement login API with credential validation and tests

--- services/auth-service/src/controllers/auth.controler.js
+++ services/auth-service/src/controllers/auth.controler.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   const { name, email, password, role } = req.body;
@@ -29,6 +30,7 @@ const registerController = async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        addresses: user.addresses,
       },
     });
   } catch (error) {
@@ -38,6 +40,61 @@ const registerController = async (req, res) => {
   }
 };
 
+const loginController = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = JWT.sign(
+      {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      message: "Logged in successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        addresses: user.addresses,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
 module.exports = {
   registerController,
+  loginController,
 };
--- services/auth-service/src/routes/auth.routes.js
+++ services/auth-service/src/routes/auth.routes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
-const { registerController } = require("../controllers/auth.controler");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/auth.controler");
 const {
   registerUserValidations,
+  loginUserValidations,
 } = require("../middlewares/validator.middleware");
 
 const router = express.Router();
 
 router.post("/register", registerUserValidations, registerController);
+router.post("/login", loginUserValidations, loginController);
 
 module.exports = router;
