c6ed84d3b55e7e34cb676f954ef3d5f0217efbf9
NimeshGhag <ghagnimesh490@gmail.com>
2026-05-11 12:22:35 +0530

    feat(auth): add current user profile endpoint and auth middleware

--- services/auth-service/src/controllers/auth.controler.js
+++ services/auth-service/src/controllers/auth.controler.js
@@ -97,6 +97,7 @@ const loginController = async (req, res) => {
 const getCurrentUserController = async (req, res) => {
   return res.status(200).json({
     message: "Current user fetched successfully",
+    
     user: req.user,
   });
 };
