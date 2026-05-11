2c552bbc19998ffaf6651c1e8194de1c9cfd4257
NimeshGhag <ghagnimesh490@gmail.com>
2026-05-11 12:20:07 +0530

    test(auth): add integration tests for current user endpoint

--- services/auth-service/src/routes/auth.routes.js
+++ services/auth-service/src/routes/auth.routes.js
@@ -3,15 +3,22 @@ const express = require("express");
 const {
   registerController,
   loginController,
+  getCurrentUserController,
 } = require("../controllers/auth.controler");
 const {
   registerUserValidations,
   loginUserValidations,
 } = require("../middlewares/validator.middleware");
 
+const authMiddleware  = require("../middlewares/auth.middleware");
+
 const router = express.Router();
 
 router.post("/register", registerUserValidations, registerController);
 router.post("/login", loginUserValidations, loginController);
+
+//GET API'secure
+
+router.get("/me", authMiddleware, getCurrentUserController);
 
 module.exports = router;
