4ba166aa088d3a18a119e21670cf5a78f830e525
NimeshGhag <ghagnimesh490@gmail.com>
2026-05-12 12:26:41 +0530

    feat(auth): implement logout with Redis token blacklisting

--- services/auth-service/src/routes/auth.routes.js
+++ services/auth-service/src/routes/auth.routes.js
@@ -4,18 +4,21 @@ const {
   registerController,
   loginController,
   getCurrentUserController,
+  logutController,
 } = require("../controllers/auth.controler");
 const {
   registerUserValidations,
   loginUserValidations,
 } = require("../middlewares/validator.middleware");
 
-const authMiddleware  = require("../middlewares/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.post("/register", registerUserValidations, registerController);
 router.post("/login", loginUserValidations, loginController);
+
+router.post("/logout", logutController);
 
 //GET API'secure
 
