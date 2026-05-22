b3fd8cf37d20378d10158bd24504b7f99a600a78
NimeshGhag <ghagnimesh490@gmail.com>
2026-05-22 12:42:18 +0530

    feat(product): implement product listing with search, pagination, and integration tests

--- services/product-service/src/models/product.model.js
+++ services/product-service/src/models/product.model.js
@@ -1,47 +1,54 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  price: {
-    amount: {
-      type: Number,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      amount: {
+        type: Number,
+        required: true,
+      },
+      currency: {
+        type: String,
+        enum: ["USD", "INR"],
+        default: "INR",
+      },
+    },
+
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-    currency: {
+    images: [
+      {
+        url: String,
+        thumbnail: String,
+        id: String,
+      },
+    ],
+    catagory: {
       type: String,
-      enum: ["USD", "INR"],
-      default: "INR",
+      enum: ["Electronics", "Books", "Fashion"],
+      required: true,
+      trim: true,
     },
   },
-
-  seller: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  images: [
-    {
-      url: String,
-      thumbnail: String,
-      id: String,
-    },
-  ],
-  catagory: {
-    type: String,
-    enum: ["Electronics","Books","Fashion"],
-    required: true,
-    trim: true,
-  },
+  { timestamps: true },
+);
+productSchema.index({
+  title: "text",
+  description: "text",
+  catagory: "text",
 });
-productSchema.index({ title: "title", description: "description", catagory: "catagory" });
 
 const productModel = mongoose.model("product", productSchema);
 
